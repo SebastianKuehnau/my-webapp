1a7c13c02efcf230113e14bf532c8d5d40c0f422
Sebastian Kuehnau <seb@vaadin.com>
2020-08-19 09:05:07 +0200

    remove solution files

--- src/main/java/org/example/MainView.java
+++ src/main/java/org/example/MainView.java
@@ -10,8 +10,7 @@ import com.vaadin.flow.router.Route;
 /**
  * The main view contains a button and a click listener.
  */
-@Route(value = "main", layout = MainLayout.class)
-@RouteAlias(value = "", layout = MainLayout.class)
+@Route
 public class MainView extends VerticalLayout {
 
     public MainView() {
